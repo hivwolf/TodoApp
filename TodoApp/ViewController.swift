9ea2d1083d8efb854b0516d3275ca4b7ea71e8f9
Elmin Delibasic <elmin.delibasic@virginpulse.com>
2019-06-27 07:49:53 -0700

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Elmin Delibasic on 6/27/19.
+//  Copyright © 2019 Elmin Delibasic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
